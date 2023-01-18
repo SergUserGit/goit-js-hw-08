565bfe034a839bccb4ca0ae5d7b57992f5b0ee62
Sergii Podriezienko <podreenko1986@gmail.com>
2023-01-18 12:55:24 +0200

    part two
    part two

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on('timeupdate', throttle(onSetCurrentTime, 1000));
+
+function onSetCurrentTime(data) {
+  localStorage.setItem(CURRENT_TIME_KEY, data.seconds);
+}
+
+player
+  .setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
